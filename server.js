d6fba1a6225b06827f2483edea1b6810f71a7713
Daniel Riano <daherian@gmail.com>
2017-05-16 13:06:42 +0200

    remove files

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function (request, response) {
-    console.log(request);
-    response.write('Hello!');
-    response.end();
-});
-
-server.listen(3000);
