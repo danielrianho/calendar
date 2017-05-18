36c23e0f58854c2b1046afffd40c34fe48962f86
Tobias Wiklund <tobias.wiklund@gmail.com>
2017-05-18 09:12:11 +0200

    new database

--- calendar.sql
+++ calendar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: localhost:8889
--- Tid vid skapande: 18 maj 2017 kl 08:31
+-- Tid vid skapande: 18 maj 2017 kl 09:11
 -- Serverversion: 5.6.35-log
 -- PHP-version: 7.1.1
 
@@ -34,6 +34,34 @@ CREATE TABLE `events` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
+-- Dumpning av Data i tabell `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `start`, `end`) VALUES
+(39, 'wew', '2017-05-16 11:00:00', '2017-05-16 01:00:00'),
+(40, 'gay', '2017-05-18 10:00:00', '2017-05-18 12:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `events_sound`
+--
+
+CREATE TABLE `events_sound` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumpning av Data i tabell `events_sound`
+--
+
+INSERT INTO `events_sound` (`id`, `title`, `start`, `end`) VALUES
+(1, 'ljud', '2017-05-15 08:00:00', '2017-05-15 10:00:00');
+
+--
 -- Index för dumpade tabeller
 --
 
@@ -44,6 +72,12 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index för tabell `events_sound`
+--
+ALTER TABLE `events_sound`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
@@ -51,7 +85,12 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT för tabell `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+--
+-- AUTO_INCREMENT för tabell `events_sound`
+--
+ALTER TABLE `events_sound`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
